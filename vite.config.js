7eb6a3f0cd0d4df1ab583a5271b23cea4d6f7a09
Diegho <dieghonm@gmail.com>
2026-03-09 21:50:23 -0300

    10 itens fandom lider

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // ⚠️ ALTERE "catalogo-produtos" para o nome exato do seu repositório no GitHub!
-const REPO_NAME = 'catalogo-produtos'
+const REPO_NAME = 'catalogo'
 
 export default defineConfig({
   plugins: [react()],
